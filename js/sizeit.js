f18113eeb9d48f5bbdb41106e0f10535ed59743c
monica <notwaldorf@gmail.com>
2013-02-28 22:58:44 -0500

    add new cards!
    this really needs some view bindings very soon.

--- js/sizeit.js
+++ js/sizeit.js
@@ -1,16 +1,43 @@
 $(function() {
-    $( "[id^='column-']" ).sortable({
-      connectWith: ".draggable",
-      items: 'li:not(.column-title)',
-      opacity: 0.6,
-      placeholder: 'ghost',
-      forcePlaceholderSize: true,
-      'start': function(event, ui) {
-      	// for some reason the placeholder style isn't actually applied
-      	ui.placeholder.css("background-color", "#b5d7d5");
-      	ui.placeholder.css("border", "none")
-      }
+  // event handlers
+  $('#new-card-box').hide();
 
-    }).disableSelection();
+  $('#add-new-card').click(function(){
+    $("#new-card-box").show('fast');
   });
+  
+  $('#cancel-card').click(function(){
+    $('#card-text').val(' ');
+    $("#new-card-box").hide('fast');
+  });
+
+  function addNewCard() {
+    var contents = $('#card-text').val();
+    $('#column-nosize').append('<li>'+ contents + '</li>')
+    $("#new-card-box").hide('fast');
+  }
+
+  $('#save-card').click(addNewCard);
+  $('#card-text').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+      addNewCard();
+    }
+  });
+
+
+  // drag and drop
+  $( "[id^='column-']" ).sortable({
+    connectWith: ".draggable",
+    items: 'li:not(.no-drag)',
+    opacity: 0.6,
+    placeholder: 'ghost',
+    forcePlaceholderSize: true,
+    'start': function(event, ui) {
+    	// for some reason the placeholder style isn't actually applied
+    	ui.placeholder.css("background-color", "transparent");
+      ui.placeholder.css("border-style", "dashed");
+    }
+  }).disableSelection();
+});
 
