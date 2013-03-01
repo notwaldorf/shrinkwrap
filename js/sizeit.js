9b43f1efa19fcd18e945a0ce448f9cd5ad4bc919
monica <notwaldorf@gmail.com>
2013-03-01 12:03:03 -0500

    added loading/storing to localStorage

--- js/sizeit.js
+++ js/sizeit.js
@@ -1,5 +1,26 @@
 $(function() {
-  // event handlers
+  var CardSizes = {
+                  XS : "xs", 
+                  S: "s", 
+                  M: "m", 
+                  L: "l", 
+                  XL: "xl",
+                  TO_DO: "todo"
+                  };
+
+  var CARD_STORAGE = "sizeit.cards";
+  var columns = [
+                    '#column-xs',
+                    '#column-s',
+                    '#column-m',
+                    '#column-l',
+                    '#column-xl',
+                    '#column-todo']
+
+  // ==========================
+  // Event handlers
+  // ==========================
+
   $('#new-card-box').hide();
 
   $('#add-new-card').click(function(){
@@ -10,13 +31,16 @@ $(function() {
   $('#clear-cards').click(clearStorage);
   
   $('#cancel-card').click(function(){
-    $('#card-text').val(' ');
+    $('#card-text').val('');
     $("#new-card-box").hide('fast');
   });
 
   $('#save-card').click(addNewCard);
 
-  // drag and drop
+  // ==========================
+  // Drag and drop
+  // ==========================
+
   $( "[id^='column-']" ).sortable({
     connectWith: ".draggable",
     items: 'li:not(.no-drag)',
@@ -30,13 +54,70 @@ $(function() {
     }
   }).disableSelection();
 
+  // for testing only
+  if (localStorage[CARD_STORAGE] == undefined)
+    knitFakeSocks();
+
+  loadAllStorage();
+
+
+  // ==========================
+  // Display cards
+  // ==========================
+
   function addNewCard() {
     var contents = $('#card-text').val();
-    putSockInDrawer(contents);
-    $('#column-nosize').append('<li>'+ contents + '</li>')
+    putSockInDrawer(contents, CardSizes.TO_DO);
+    $('#column-todo').append('<li class="card">'+ contents + '</li>');
     $("#new-card-box").hide('fast');
   }
 
+  function displayCards(cards) {
+    // first clear everything
+    for (var i = 0; i < columns.length; i++) {
+      var jCol = $(columns[i]);
+      jCol.children().remove(".card");  
+    }
+
+    if (cards == null)
+      return;
+
+    for (var i = 0; i < cards.length; i++) {
+      var jCol = $('#column-' + cards[i].size);
+      jCol.append('<li class="card">'+ cards[i].text + '</li>');  
+    }
+  }
+
+  // ==========================
+  // Save and load cards
+  // ==========================
+
+  function loadAllStorage() {
+    var stored = localStorage[CARD_STORAGE];  
+
+    if (stored == undefined)
+      var cards = []
+    else
+      cards = JSON.parse(stored);
+
+    displayCards(cards);
+  }
+
+  function clearStorage() {
+    localStorage.removeItem(CARD_STORAGE);  
+    displayCards(null);
+  }
+
+  function putSockInDrawer(sock, size) {
+    var stored = localStorage[CARD_STORAGE];
+    if (stored == undefined)
+      var cards = []
+    else
+      cards = JSON.parse(stored);
+
+    cards.push({text: sock, size: size});
+    localStorage[CARD_STORAGE] = JSON.stringify(cards);
+  }
 
   function hasStorageSkills() {
     try {
@@ -46,15 +127,20 @@ $(function() {
     }
   }
 
-  function loadAllStorage() {
-  }
+  // ==========================
+  // Helper elves
+  // ==========================
 
-  function clearStorage() {
-  }
+  function knitFakeSocks() {
+    var socks = [];
+    socks.push({text: "one", size: CardSizes.XS});
+    socks.push({text: "two", size: CardSizes.TO_DO});
+    socks.push({text: "three", size: CardSizes.TO_DO});
+    socks.push({text: "four", size: CardSizes.TO_DO});
 
-  function putSockInDrawer(sock) {
+    localStorage.removeItem(CARD_STORAGE);
+    localStorage[CARD_STORAGE] = JSON.stringify(socks);
   }
-
 
 });
 
